cd84bb5f2bec837b9b9e6800ff09e2e3b30ffb8d
armaansharma19 <122995437+armaansharma19@users.noreply.github.com>
2023-08-29 11:18:28 +0530

    Add files via upload

---
+++ qsort.c
@@ -0,0 +1,23 @@
+//sorting
+#include<stdio.h>
+#include<stdlib.h>
+int asc(const void *a,const void *b){
+    return(*(int *)a - *(int *)b);
+}
+int main(){
+    int p;
+    int arr[1000];
+   // int size= sizeof(arr)/sizeof(arr[0]);
+    printf("enter number of elements ");
+    scanf("%d",&p);
+    printf("enter values in array ");
+    for(int j;j<p;j++){
+        scanf("%d",&arr[j]);
+    }
+    
+    qsort(arr,p,sizeof(int),asc);
+    printf("asc\n");
+    for(int i =0;i< p;i++){
+        printf("%d ",arr[i]);
+    }
+}
