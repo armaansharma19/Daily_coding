4002d1ce2b8cceec6dd87d4fa3de3abf0d51fb11
armaansharma19 <122995437+armaansharma19@users.noreply.github.com>
2023-08-30 11:56:54 +0530

    Add files via upload

---
+++ stackpush.c
@@ -0,0 +1,39 @@
+//stack
+#include <stdio.h>
+#define maxsize 10
+int top=-1;
+int stack[maxsize];
+int full(){
+    return top==(maxsize-1);
+}
+int isempty(){
+    return top==-1;
+}
+int push(int ele){
+    if(full()){
+        printf("Stack overflow\n");
+        return 0;
+    }
+    stack[++top]=ele;
+}
+int pop(){
+    if(isempty()){
+        printf("Stack Underflow");
+    }
+    return stack[top--];
+}
+int display(){
+    for(int i=0;i<=top;i++){
+        printf("%d,",stack[i]);
+    }
+}
+
+int main(){
+    push(10);
+    push(102);
+    push(120);
+    push(105);
+    push(104);
+
+    display();
+}
